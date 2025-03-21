1548eb6454cf45a7f558adb36afb22e478855c78
jigmat dorjey <jigmatdorjey255@gmail.com>
2025-03-21 11:57:20 +0530

    home to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Route ,Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home'
 import EmailVerify from './pages/EmailVerify'
 import ResetPassword from './pages/ResetPassword'
 import Navbar from './components/Navbar'
